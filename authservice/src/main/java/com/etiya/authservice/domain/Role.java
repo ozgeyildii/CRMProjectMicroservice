b2d11aff98529f13a344ff9aaaa6f679f20e5c75
ahmet.kazan <ahmettarkazan@gmail.com>
2025-10-27 16:55:29 +0300

    added auth service

---
+++ authservice/src/main/java/com/etiya/authservice/domain/Role.java
@@ -0,0 +1,46 @@
+package com.etiya.authservice.domain;
+
+import com.etiya.common.entities.BaseEntity;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "roles")
+public class Role extends BaseEntity implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "role",cascade = CascadeType.ALL)
+    private Set<UserRole> userRoles;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.name.toUpperCase(); //ADMIN
+    }
+}
---
+++ common/src/main/java/com/etiya/common/jwt/JwtService.java
@@ -0,0 +1,69 @@
+package com.etiya.common.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret.key}")
+    private String SECRET_KEY;
+
+    @Value("${jwt.expiration}")
+    private long EXPIRATION;
+
+    public String generateToken(String username, List<String> roles){
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("roles",roles);
+        return createToken(claims,username);
+    }
+
+    public Boolean validateToken(String token,String username){
+        return username.equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+
+    public String extractUsername(String token){
+        return getClaimsFromToken(token).getSubject();
+    }
+
+    public List<String> extractRoles(String token){
+        return getClaimsFromToken(token).get("roles",List.class);
+    }
+
+    private Date extractExpiration(String token){
+        return getClaimsFromToken(token).getExpiration(); //10/27/2025 4:08 PM
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Claims getClaimsFromToken(String token){
+        return Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token).getPayload();
+    }
+
+    private String createToken(Map<String,Object> claims,String username){
+        return Jwts.builder().claims(claims).subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(getSigningKey(),Jwts.SIG.HS256)
+                .compact();
+    }
+
+    private SecretKey getSigningKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+}
